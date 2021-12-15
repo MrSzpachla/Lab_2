7f4d42d82f2408a88fe4b6a26d677b738c5dc115
MrSzpachla <kamilpaszkiet@gmail.com>
2021-12-15 10:40:04 +0100

    tak

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
@@ -8,11 +9,28 @@ public class Main {
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
+        for(int i = 100; i > -1; i--){
+            if(i%50 ==0)
+            {
+                System.out.println("liczba "+i+" podzielna przez 50");
+            }
+        }
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
 
+        int[] tablica = new int[5];
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0;i<5;i++)
+        {
+            System.out.println("Wpisz "+ i+" liczbę:");
+            tablica[i] = scan.nextInt();
+        }
+        for(int zmienna : tablica)
+        {
+            System.out.print((zmienna+11)+" ");
+        }
 
     }
 }
